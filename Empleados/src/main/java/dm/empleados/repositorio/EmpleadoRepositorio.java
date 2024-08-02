dd20489c83993859df57080f751f437034a6dff4
David Murillo <david.josue.murillo23@gmail.com>
2024-08-02 14:52:53 -0500

    Creación de capa de servicio y datos

---
+++ Empleados/src/main/java/dm/empleados/repositorio/EmpleadoRepositorio.java
@@ -0,0 +1,7 @@
+package dm.empleados.repositorio;
+
+import dm.empleados.modelo.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpleadoRepositorio extends JpaRepository<Empleado, Integer> {
+}
