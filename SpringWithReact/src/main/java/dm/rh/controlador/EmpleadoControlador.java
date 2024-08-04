c61290b1627ee937728d68524dc84772c1e5e84c
David Murillo <david.josue.murillo23@gmail.com>
2024-08-04 15:26:20 -0500

    Montando frontend en React+vite

---
+++ SpringWithReact/src/main/java/dm/rh/controlador/EmpleadoControlador.java
@@ -0,0 +1,30 @@
+package dm.rh.controlador;
+
+import dm.rh.modelo.Empleado;
+import dm.rh.servicio.EmpleadoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("rh-app") // (http:/localhost/8080/rh-app/
+@CrossOrigin(value = "http://localhost:4200")
+public class EmpleadoControlador {
+    private static final Logger logger = LoggerFactory.getLogger(EmpleadoControlador.class);
+
+    @Autowired
+    EmpleadoServicio empleadoServicio;
+
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados() {
+        List<Empleado> empleados = empleadoServicio.listaEmpleados();
+        empleados.forEach((empleado) -> logger.info(empleado.toString()));
+        return empleados;
+    }
+}
