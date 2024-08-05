2eb6413fdfd2e5bc02a903224b6ac9553dc4fd84
David Murillo <david.josue.murillo23@gmail.com>
2024-08-04 19:58:57 -0500

    Vinculando enlaces

--- App-React/sistema-contactos-app/src/main.jsx
+++ App-React/sistema-contactos-app/src/main.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Contact } from './Contact'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Nav } from './components/Nav'
+import { NewContact } from './NewContact'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Contact />
-  </React.StrictMode>,
+   <React.StrictMode>
+      <BrowserRouter>
+         <Nav />
+
+         <Routes>
+            <Route path="/" element={<Contact />} />
+            <Route path="/agregar" element={<NewContact />} />
+            <Route path="/editar/:id" element={<NewContact />} />
+         </Routes>
+      </BrowserRouter>
+   </React.StrictMode>,
 )
