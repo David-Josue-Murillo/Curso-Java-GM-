71334cae95f7d04b28b36e701aeab743cac89d91
David Murillo <david.josue.murillo23@gmail.com>
2024-08-04 20:10:07 -0500

    maquetacion al componente <ListContact>

--- App-React/sistema-contactos-app/src/Contact.jsx
+++ App-React/sistema-contactos-app/src/Contact.jsx
@@ -1,6 +1,48 @@
+import { Link } from "react-router-dom"
 
 export const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+   return (
+      <>
+         <div className='container'>
+            <div className='container text-center' style={{ margin: "30px" }}>
+               <h3 className='display-5 fw-bold text-center my-3'>Sistema de Contactos</h3>
+            </div>
+
+            <div className='container'>
+               <table className='table table-striped table-hover align-middle table-border'>
+                  <thead className='table-dark text-center'>
+                     <tr>
+                        <th scope='col'>Id</th>
+                        <th scope='col'>Nombre</th>
+                        <th scope='col'>Celular</th>
+                        <th scope='col'>Email</th>
+                        <th scope='col'>Accion</th>
+                     </tr>
+                  </thead>
+                  <tbody className="text-center">
+                     <tr>
+                        <th scope='row'>1</th>
+                        <td>Nombre</td>
+                        <td>Celular</td>
+                        <td>Email</td>
+                        <td className='d-flex justify-content-center gap-3'>
+                           <Link
+                              to='/editar/1'
+                              className='btn btn-outline-warning btn-sm'>
+                              Modificar
+                           </Link>
+
+                           <button
+                              data-id={1}
+                              className='btn btn-outline-danger btn-sm'>
+                              Eliminar
+                           </button>
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+         </div>
+      </>
+   )
 }
