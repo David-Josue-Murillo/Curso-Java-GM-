13525f168f9806806ad97b213d2c48589721a7b3
David Murillo <david.josue.murillo23@gmail.com>
2024-08-04 20:06:08 -0500

    maquetacion al componente <Nav>

--- App-React/sistema-contactos-app/src/components/Nav.jsx
+++ App-React/sistema-contactos-app/src/components/Nav.jsx
@@ -1,6 +1,27 @@
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <>
+        <div className='container'>
+            <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+                <div className='container-fluid'>
+                    <a className='navbar-brand' href='/'>Sistema de Recusrsos Humanos</a>
+                    <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                        <span className='navbar-toggler-icon'></span>
+                    </button>
+                    <div className='collapse navbar-collapse' id='navbarNav'>
+                        <ul className='navbar-nav'>
+                            <li className='nav-item'>
+                                <a className='nav-link active' aria-current='page' href='/'>Inicio</a>
+                            </li>
+                            <li className='nav-item'>
+                                <a className='nav-link' href='/agregar'>Agregar Empleado</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>  
+        </div>
+    </>
   )
 }
