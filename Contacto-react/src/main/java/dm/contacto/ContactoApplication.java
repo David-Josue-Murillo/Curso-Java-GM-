b5821b3fd3a05f24bbded64c7f7c97bc92970fd3
David Murillo <david.josue.murillo23@gmail.com>
2024-08-04 19:35:15 -0500

    Inicializando nuevo proyecto

---
+++ Contacto-react/src/main/java/dm/contacto/ContactoApplication.java
@@ -0,0 +1,13 @@
+package dm.contacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactoApplication.class, args);
+	}
+
+}
